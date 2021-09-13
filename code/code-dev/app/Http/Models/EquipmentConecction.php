0feb844e1370f6f234acc83e2aca660146efcd5e
Daniel Velasquez <42074978+dvlsqz@users.noreply.github.com>
2021-09-13 10:16:48 -0600

    update equipment delete conecctions 13092021

--- code/code-dev/app/Http/Models/EquipmentConecction.php
+++ code/code-dev/app/Http/Models/EquipmentConecction.php
@@ -4,13 +4,17 @@ namespace App\Http\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EquipmentConecction extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 
     protected $table = 'equipment_conecctions';
-    protected $hidden = ['created_at', 'updated_at'];
+    protected $hidden = ['created_at', 'updated_at']; 
 
     public function equipment(){
         return $this->hasOne(Equipment::class,'id','idequipment_conecction');
